834838d261a46a6504b2c0411038ed22660621fd
jsanchez92 <jiubersanchez@gmail.com>
2024-05-22 16:05:41 -0600

    creacion de migraciones y componentes

---
+++ app/Http/Controllers/MedicationsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Medications;
+use Illuminate\Http\Request;
+
+class MedicationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Medications $medications)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Medications $medications)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Medications $medications)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Medications $medications)
+    {
+        //
+    }
+}
---
+++ app/Livewire/MedicationDetails.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MedicationDetails extends Component
+{
+    public function render()
+    {
+        return view('livewire.medication-details');
+    }
+}
